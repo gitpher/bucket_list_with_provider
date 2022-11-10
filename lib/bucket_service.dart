e33d48e3878c29b634c843fef4324d43a8a6ed78
gitpher <jun_chois@naver.com>
2022-11-11 00:05:25 +0900

    creating bucket with provider

--- lib/bucket_service.dart
+++ lib/bucket_service.dart
@@ -7,4 +7,10 @@ class BucketService extends ChangeNotifier {
   List<Bucket> bucketList = [
     Bucket('잠자기', false), // 더미(dummy) 데이터
   ];
+
+  /// bucket 추가
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+    notifyListeners();
+  }
 }
